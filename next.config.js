db0d100e3126b28e78bd4a026f848625be6b8f01
maikofelix47 <maikofelix89@gmail.com>
2023-08-30 12:51:09 +0300

    Added prisma

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const nextConfig = (phase) => {
+  return {
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL,
+    },
+  };
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
